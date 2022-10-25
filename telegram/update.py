481769a4efcaa0f7fc6caa4740b6de3fef18e36f
Jalilov <javohirjalilov777@gmail.com>
2022-10-25 13:48:37 +0500

    update

---
+++ telegram/update.py
@@ -0,0 +1,26 @@
+import json
+from .message import Message
+class Update:
+    def __init__(self, update) -> None:
+        self.update_id = update['update_id']
+        self.message   = Message(update['message'])
+
+    
+    def fromDict(self)->dict:
+        '''
+        Convert User object to dictionary
+        Returns:
+            dict: dictionary of user data
+        '''
+        update_dict = {
+            'update_id': self.update_id,
+            'message': self.message.fromDict()
+        }
+        return update_dict
+
+    #Override the __str__ method to print the user data
+    def __str__(self):
+        '''
+        Print the user data
+        '''
+        return json.dumps(self.fromDict())
