c543b089a57790534dc1dbaa5f1858a2c72cc283
Jalilov <javohirjalilov777@gmail.com>
2022-10-25 15:54:54 +0500

    get chat_id from getUpdates

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from turtle import update
 import telegram
+from telegram import chat
 TOKEN='5559122728:AAHOu1gL4pA1riJPMCmICNTKI57P5xnHsyA'
 
 bot = telegram.Bot(TOKEN)
-print(bot.getMe())
+
+updates = bot.getUpdates()
+
+chat_id = updates[-1].message.chat.id
+text = updates[-1].message.text
+
+bot.sendMessage(chat_id, text)
